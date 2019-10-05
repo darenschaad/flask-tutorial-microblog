ffc34fe0225a3a8d55f796fe500a48fb413047f1
Daren Schaad <darenschaad@gmail.com>
2019-10-04 19:07:07 -0700

    basic login form is showing up on the login route

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm)
+class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Passowrd', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
